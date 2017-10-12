de3bc3db36ae5c4bd9a04115a84a977028043a92
Craig <craig@oppkey.com>
2017-10-11 17:58:34 -0700

    added stamp example

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ SCREENHEIGHT = 720
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 drawingArea = pygame.Rect(SCREENWIDTH/4, 0, SCREENWIDTH/4 * 3, SCREENHEIGHT)
 
 toolbox = tools.Box((SCREENWIDTH/4, SCREENHEIGHT))
 
-color = GREEN
+panda = pygame.image.load("img/panda.png")
+
+brushColor = GREEN
+brushSize = 5
+stamp = False
 mousedown = False
 
 gameOn = True
@@ -30,11 +35,28 @@ while gameOn:
 
     if mousedown:
         if drawingArea.collidepoint(mouse_pos):
-            pygame.draw.circle(SCREEN, color, mouse_pos, 5)
+            if not stamp:
+                pygame.draw.circle(SCREEN, brushColor, mouse_pos, brushSize)
+            else:
+                SCREEN.blit(panda, mouse_pos)
+
         for button in toolbox.buttons:
             if button.rect.collidepoint(mouse_pos):
+                if button.type == "stamp":
+                    stamp = True
+                else:
+                    stamp = False
                 if button.name == "green":
-                    color = GREEN
+                    brushColor = GREEN
+                elif button.name == "white":
+                    brushColor = WHITE
                 elif button.name == "eraser":
-                    color = BLACK
+                    brushColor = BLACK
+                elif button.name == "small":
+                    brushSize = 5
+                elif button.name == "medium":
+                    brushSize = 12
+                elif button.name == "large":
+                    brushSize = 20
+
     pygame.display.update()
--- tools.py
+++ tools.py
@@ -3,6 +3,8 @@ import pygame
 GRAY = (30, 30, 30)
 LIGHTGRAY = (60, 60, 60)
 GREEN = (10, 200, 10)
+WHITE = (255, 255, 255)
+
 SIZE = 128
 
 
@@ -15,28 +17,77 @@ class Box:
         self.buttons = pygame.sprite.Group()
         self.eraser()
         self.green()
+        self.white()
+        self.small()
+        self.medium()
+        self.large()
+        self.panda()
         self.buttons.draw(self.mainPanel)
                 
     def eraser(self):
-        button = Button(pygame.image.load("img/eraser.png"))
+        button = Button()
+        button.image = pygame.image.load("img/eraser.png")
         button.rect.x = 30
         button.rect.top = 30
         button.name = "eraser"
         self.buttons.add(button)
 
-    def green(self):
-        buttonSurface = pygame.Surface((80, 80))
-        buttonSurface.fill(GREEN)
-        button = Button(buttonSurface)
+    def large(self):
+        button = Button()
+        pygame.draw.circle(button.image, WHITE, (40, 40), 20)
         button.rect.x = 170
         button.rect.top = 30
+        button.name = "large"
+        self.buttons.add(button)
+
+    def small(self):
+        button = Button()
+        pygame.draw.circle(button.image, WHITE, (40, 40), 5)
+        button.rect.x = 30
+        button.rect.top = 158
+        button.name = "small"
+        self.buttons.add(button)
+
+    def medium(self):
+        button = Button()
+        pygame.draw.circle(button.image, WHITE, (40, 40), 12)
+        button.rect.x = 170
+        button.rect.top = 158
+        button.name = "medium"
+        self.buttons.add(button)
+
+    def green(self):
+        button = Button()
+        button.image.fill(GREEN)
+        button.rect.x = 170
+        button.rect.top = 286
         button.name = "green"
         self.buttons.add(button)
-    
+
+    def white(self):
+        button = Button()
+        button.image.fill(WHITE)
+        button.rect.x = 30
+        button.rect.top = 286
+        button.name = "white"
+        self.buttons.add(button)
+
+    def panda(self):
+        button = Button()
+        button.image = pygame.image.load("img/panda.png")
+        button.rect.x = 30
+        button.rect.top = 414
+        button.type = "stamp"
+        button.name = "panda"
+        self.buttons.add(button)
+
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
+        self.image = pygame.Surface((80, 80))
+        self.image.fill(GRAY)
         self.rect = self.image.get_rect()
+        self.type = ""
+        self.name = ""
         
